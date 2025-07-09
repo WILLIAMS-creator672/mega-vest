6f8d533bcc5d2490c7b574c702c224341c0ce701
WILLIAMS-creator672 <wisdomwills1@outlook.com>
2025-07-09 13:13:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let mobile = (value) => {
+    document.getElementById('menu').classList.toggle('left-0')
+    document.getElementById('hamburger').classList.toggle('hamburger-active');
+}
+
+let openUser = () => {
+    document.getElementById('user').classList.toggle('block')
+}
+
